de3d3d8954e8edbb8d3aab8cb0caafaaa7723256
aaron47 <aaronborgi1@gmail.com>
2022-07-24 18:11:33 +0100

    first commit

---
+++ src/components/Banner.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image"
+
+const Banner: React.FC = () => {
+  return (
+    <div>
+      {/* <div>
+        <Image />
+      </div> */}
+    </div>
+  )
+}
+
+export default Banner
